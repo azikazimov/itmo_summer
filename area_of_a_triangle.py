5c181f0ea4498c48c90393186b919e163ea22e9f
azikazimov <46689166+azikazimov@users.noreply.github.com>
2020-06-15 23:52:34 +0300

    Update area_of_a_triangle.py
    Теперь для (1, 2, 3) тоже не работает

--- area_of_a_triangle.py
+++ area_of_a_triangle.py
@@ -6,6 +6,8 @@ def plos(a, b, c):
     try:
         p = (a + b + c) / 2
         s = math.sqrt(p * (p - a) * (p - b) * (p - c))
+        if s == 0:
+            return 'ай, ай. какой-то неправильный треугольник.'
         return s
     except ValueError:
         return 'ай, ай. какой-то неправильный треугольник.'
