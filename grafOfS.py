6ea6e5b45aa02d13872b6047ff9793283fa49071
azikazimov <46689166+azikazimov@users.noreply.github.com>
2020-06-16 00:03:39 +0300

    update for axis
    передвинул оси

--- grafOfS.py
+++ grafOfS.py
@@ -13,6 +13,10 @@ def grafOfMove(v0, a, t):
     seconds = np.linspace(0, t, maxSeconds)
     s = [(v0 * t + (a * (sec**2)) / 2) for sec in seconds]
     plt.plot(seconds, s)
-    plt.show()
+
+    ax = plt.gca()
+    ax.spines['bottom'].set_position('center')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
 
 grafOfMove(24, 1, 12) # указать в порядке : v0, a, t
